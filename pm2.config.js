54273cb9076ad43e0947543f4fa0758800de2075
hafidzyami <18221074@std.stei.itb.ac.id>
2025-09-30 13:40:59 +0700

    feat: remove port from pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -12,12 +12,10 @@ module.exports = {
       
       env_dev: {
         NODE_ENV: "development",
-        PORT: 3070,
       },
 
       env_prod: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
     },
   ],
